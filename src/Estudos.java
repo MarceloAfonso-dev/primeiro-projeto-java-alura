ace32835ef53f99ac059ce5822c3f68136b9d37c
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-05-09 14:46:23 -0300

    Arquivos aula 1

---
+++ src/Estudos.java
@@ -0,0 +1,5 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("O código fonte Java é compilado para bytecode e depois interpretada pela JVM em multiplataforma.");
+    }
+}
---
+++ src/Perfil.java
@@ -0,0 +1,6 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Marcelo!");
+        System.out.println("Tudo bem?");
+    }
+}
